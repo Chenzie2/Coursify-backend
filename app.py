87efb6cc4c8c2fb57ea4f1bcb1faf9ee1789a854
g_zawie <gzawie24@gmail.com>
2025-06-19 18:51:09 +0300

    Initial flask app setup

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+
+
+app = Flask(__name__)
+    
+    # Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coursify.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    # Initialize extensions
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+CORS(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)   
+
