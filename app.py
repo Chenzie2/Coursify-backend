0d4ff4192fea827d1ed38d633a97bdfb2d992689
jedaqsaul <jedaqsaul@gmail.com>
2025-06-20 21:49:05 +0300

    Add POST /courses route to allow creation of new courses

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, make_response, request
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_restful import Api, Resource
 
-from models import db, User, Course, Enrollment, Review
+from models import db, Course
+
 
 
 
@@ -16,7 +17,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coursify.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     # Initialize extensions
-db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 CORS(app)
 db.init_app(app)
@@ -31,6 +32,44 @@ class Courses(Resource):
         courses=Course.query.all()
         course_list=[course.to_dict() for course in courses]
         return make_response(course_list, 200)
+    
+    def post(self):
+        try:
+            data = request.get_json()
+            title = data.get("title")
+            description = data.get("description")
+            duration = data.get("duration")
+            level = data.get("level")
+            lesson_count = data.get("lesson_count")
+            instructor_id = data.get("instructor_id")
+
+            course = Course(
+                title=title,
+                description=description,
+                duration=duration,
+                level=level,
+                lesson_count=lesson_count,
+                instructor_id=instructor_id
+            )
+
+            db.session.add(course)
+            db.session.commit()
+
+            response = {
+                "status": "success",
+                "code": 201,
+                "message": "Course created successfully"
+            }
+            return make_response(response, 201)
+
+        except Exception as e:
+            response = {
+                "status": "error",
+                "code": 500,
+                "message": "Something went wrong",
+                "error": str(e)
+            }
+            return make_response(response, 500)
     
 
 
@@ -48,6 +87,10 @@ class CourseById(Resource):
                 "status":"unsuccessful"
             }, 404)
         
+
+
+
+
 
 api.add_resource(Courses, "/courses")
 api.add_resource(CourseById, "/courses/<int:id>")
