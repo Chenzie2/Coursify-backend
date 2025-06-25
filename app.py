a2b14e3a35e1dd38d7b833b8e4258b5c33b6de79
g_zawie <gzawie24@gmail.com>
2025-06-25 17:41:42 +0300

    Add Serializer and Bcrypt

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_restful import Api
+from models import db, create_app
 
 
 # Import models and routes
@@ -16,9 +17,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coursify.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     # Initialize extensions
-    db.init_app(app)
-    Migrate(app, db)
-    CORS(app)
+db.init_app(app)
+Migrate(app, db)
+CORS(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 CORS(app)
