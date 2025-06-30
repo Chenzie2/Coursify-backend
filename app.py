025bdbb727b2c666f4e642c56e545cd375646962
Joy Malinda <kokimalinda@gmail.com>
2025-06-30 22:28:14 +0300

    addition of database uri

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask_migrate import Migrate
 
 # === App Setup ===
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'dev-secret-key')
 
